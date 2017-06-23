95b954cb4bf26975eb299f315ea5af72d1fcfd01
Jad Salhani <jadsalhani@Jads-MacBook-Pro.local>
2017-06-23 10:55:30 +0300

    Testing jenkins karma conf

--- karma-jenkins.conf.js
+++ karma-jenkins.conf.js
@@ -1,9 +1,48 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
-module.exports = function (config) {
+// module.exports = function (config) {
+//   config.set({
+//     basePath: '',
+//     frameworks: ['jasmine', '@angular/cli'],
+//     plugins: [
+//       require('karma-jasmine'),
+//       require('karma-chrome-launcher'),
+//       require('karma-jasmine-html-reporter'),
+//       require('karma-coverage-istanbul-reporter'),
+//       require('@angular/cli/plugins/karma')
+//     ],
+//     client:{
+//       clearContext: false // leave Jasmine Spec Runner output visible in browser
+//     },
+//     coverageIstanbulReporter: {
+//       reports: [ 'html', 'lcovonly' ],
+//       fixWebpackSourcePaths: true
+//     },
+//     angularCli: {
+//       environment: 'dev'
+//     },
+//     reporters: ['progress', 'kjhtml', 'dots', 'junit'],
+//     junitReporter: {
+//       outputFile: 'test-results.xml'
+//     },
+//     colors: true,
+//     logLevel: config.LOG_INFO,
+//     autoWatch: false,
+//     singleRun: true
+//   });
+// };
+
+
+module.exports = function(config) {
   config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
@@ -12,23 +51,56 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+
+
+    // list of files / patterns to load in the browser
+    // files: [
+    //   'index.js',
+    //   'test/test.js'
+    // ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
     },
-    coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
-    },
-    angularCli: {
-      environment: 'dev'
-    },
-    reporters: ['progress', 'kjhtml', 'dots', 'junit'],
-    junitReporter: {
-      outputFile: 'test-results.xml'
-    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
     colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-    autoWatch: false,
-    singleRun: true
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
   });
 };
